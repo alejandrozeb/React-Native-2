11b85755f3aa6f1e121d28986e01ccb85f6a0f89
Alejandro Zeballos <alejandro.zeballos@fundacion-jala.org>
2022-12-21 20:57:22 -0400

    add touchable components

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableWithoutFeedback
+        onPress={() => Alert.alert("Hemos tocado el texto")}
+      >
+        <Text>Hola mundo</Text>
+      </TouchableWithoutFeedback>
     </View>
   );
 }

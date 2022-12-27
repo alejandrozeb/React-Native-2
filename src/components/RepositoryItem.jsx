e480f68a9ebfa95488a1c435fb044a7b29b97cd8
Alejandro Zeballos <alejandro.zeballos@fundacion-jala.org>
2022-12-27 18:18:04 -0400

    add repository item component

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const RepositoryItem = (props) => {
+  return (
+    <View
+      key={props.id}
+      style={{ padding: 20, paddingBottom: 5, paddingTop: 5 }}
+    >
+      <Text style={{ fontWeight: "bold", marginBottom: 5 }}>
+        {" "}
+        id: {props.id}
+      </Text>
+      <Text> FullName: {props.fullName}</Text>
+      <Text> Description: {props.description}</Text>
+      <Text> Language: {props.language}</Text>
+      <Text> Start: {props.stargazersCount}</Text>
+      <Text> Forks: {props.forksCount}</Text>
+      <Text> Review: {props.reviewCount}</Text>
+      <Text> Rating: {props.ratingAverage}</Text>
+      <Text> ---------------------- </Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 import { FlatList, View, Text } from "react-native";
 import repositories from "../data/repositories";
+import RepositoryItem from "./RepositoryItem";
 
 const RepositoryList = () => {
   return (
     <FlatList
       data={repositories}
       ItemSeparatorComponent={() => <Text> </Text>}
-      renderItem={({ item: repo }) => (
-        <View key={repo.id}>
-          <Text> id: {repo.id}</Text>
-          <Text> FullName: {repo.fullName}</Text>
-          <Text> Description: {repo.description}</Text>
-          <Text> Language: {repo.language}</Text>
-          <Text> Start: {repo.stargazersCount}</Text>
-          <Text> Forks: {repo.forksCount}</Text>
-          <Text> Review: {repo.reviewCount}</Text>
-          <Text> Rating: {repo.ratingAverage}</Text>
-          <Text> ---------------------- </Text>
-        </View>
-      )}
+      renderItem={({ item: repo }) => <RepositoryItem {...repo} />}
     />
   );
 };

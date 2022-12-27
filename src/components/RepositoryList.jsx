938f82bb944a907c4f96f8617bb16aa7d5bc2ca9
Alejandro Zeballos <alejandro.zeballos@fundacion-jala.org>
2022-12-27 17:50:00 -0400

    add Flatlist compnent

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { FlatList, View, Text } from "react-native";
 import repositories from "../data/repositories";
 
 const RepositoryList = () => {
   return (
-    <View>
-      {repositories.map((repo) => (
+    <FlatList
+      data={repositories}
+      ItemSeparatorComponent={() => <Text> </Text>}
+      renderItem={({ item: repo }) => (
         <View key={repo.id}>
-          <Text>{repo.id}</Text>
+          <Text> id: {repo.id}</Text>
+          <Text> FullName: {repo.fullName}</Text>
+          <Text> Description: {repo.description}</Text>
+          <Text> Language: {repo.language}</Text>
+          <Text> Start: {repo.stargazersCount}</Text>
+          <Text> Forks: {repo.forksCount}</Text>
+          <Text> Review: {repo.reviewCount}</Text>
+          <Text> Rating: {repo.ratingAverage}</Text>
+          <Text> ---------------------- </Text>
         </View>
-      ))}
-    </View>
+      )}
+    />
   );
 };
 
